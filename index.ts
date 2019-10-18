808e81b488799ee792376faa8e4ab56f8f76df15
Anwesh43 <anweshthecool0@gmail.com>
2019-10-18 19:27:24 +0530

    adding ABSNode class

--- index.ts
+++ index.ts
@@ -146,3 +146,48 @@ class Animator {
         }
     }
 }
+
+class ABSNode {
+
+    next : ABSNode
+    prev : ABSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new ABSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawABSNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ABSNode {
+        var curr : ABSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
