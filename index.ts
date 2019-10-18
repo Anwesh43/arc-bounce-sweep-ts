8277f921689bd389b57fb1c18d72eedeb0f0296c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-18 19:10:09 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -101,3 +101,27 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class State {
+
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += this.dir * scGap
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
